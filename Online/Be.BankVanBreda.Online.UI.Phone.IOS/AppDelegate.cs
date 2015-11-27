a4745e51509b42763db5d1447180f8c14faa2ef1
Leslie <leslie.bosman@bankvanbreda.be>
2015-11-27 22:15:31 +0100

    Empty Forms 1.5 app

---
+++ Online/Be.BankVanBreda.Online.UI.Phone.IOS/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using Be.BankVanBreda.Online.UI.Phone;
+
+namespace Be.BankVanBreda.Online.UI.Phone.IOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+			LoadApplication (new SmartPhoneApplication ());
+
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+
---
+++ Online/Be.BankVanBreda.Online.UI.Phone/SmartPhoneApplication.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Be.BankVanBreda.Online.UI.Phone
+{
+	public class SmartPhoneApplication : Application
+	{
+		public SmartPhoneApplication ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							XAlign = TextAlignment.Center,
+							Text = "Welcome to the forms 1.5 smartphone application!"
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
