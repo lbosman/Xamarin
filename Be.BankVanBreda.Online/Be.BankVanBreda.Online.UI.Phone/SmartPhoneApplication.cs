fe6b2b139fc143fcf458a765734e6581f43bd024
Leslie <leslie.bosman@bankvanbreda.be>
2015-11-29 21:44:03 +0100

    Start with build configs and config mappings

---
+++ Be.BankVanBreda.Online/Be.BankVanBreda.Online.UI.Phone/SmartPhoneApplication.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Be.BankVanBreda.Online.UI.Phone
+{
+	public class SmartPhoneApplication : Application
+	{
+		public SmartPhoneApplication ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							XAlign = TextAlignment.Center,
+							#if DEBUG
+							Text = "Debug Xamarin Forms!"
+							#elif PREPRODUCTION
+							Text = "PreProduction Baby!"
+							#else
+							Text = "Welcome to Xamarin Forms!"
+							#endif
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
