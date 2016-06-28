6155f5671403f7d371f51900f47325b3d1918781
Leslie <leslie.bosman@bankvanbreda.be>
2016-06-28 10:44:00 +0200

    Button in scrollview not triggering example

---
+++ EditorScrollViewButtons/EditorScrollViewButtons/EditorScrollViewButtonsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace EditorScrollViewButtons
+{
+	public partial class EditorScrollViewButtonsPage : ContentPage
+	{
+		int i = 0;
+
+		public EditorScrollViewButtonsPage ()
+		{
+			InitializeComponent ();
+		}
+
+		void Increase_Clicked (object sender, System.EventArgs e)
+		{
+			MyEditor.Text += string.Format("{0}. Increased\r\n", ++i);
+			MyEditor.HeightRequest += 100;
+		}
+
+		void Decrease_Clicked (object sender, System.EventArgs e)
+		{
+			MyEditor.Text += string.Format ("{0}. Decreased\r\n", ++i);
+			MyEditor.HeightRequest -= 100;
+		}
+	}
+}
+
